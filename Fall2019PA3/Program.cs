901c9c93d1d02aff8f05015c31288a2a8afd5391
Jeff Lucas <jslucas1@msn.com>
2019-10-04 07:54:10 -0500

    Add project files.

---
+++ Fall2019PA3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2019PA3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The key to this project is eating the elephant 1 bite at a time!
+            // Plan first and decompse the problem and it will be much easier than you think!
+        }
+    }
+}
